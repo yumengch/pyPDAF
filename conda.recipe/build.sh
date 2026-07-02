93ee47bf902132f22eaac5416d9c1b322c51a6b3
yumengch <yumeng.chen@reading.ac.uk>
2026-07-02 08:33:15 +0100

    better handling of Fortran binding header in Mac

--- conda.recipe/build.sh
+++ conda.recipe/build.sh
@@ -5,7 +5,7 @@ CC=mpicc
 FC=mpifort
 
 $PYTHON -m pip install . -v --no-build-isolation \
-    --config-settings=setup-args="-Dblas_lib=['openblas',]" \
+    --config-settings=setup-args="-Dlink_args_blas=-L"$PREFIX"/lib,-lopenblas" \
     --config-settings=setup-args="-Dincdirs="$PREFIX"/include" \
     --config-settings=setup-args="-Dlibdirs="$PREFIX"/lib" \
     --config-settings=setup-args="-Dbuildtype=release"
