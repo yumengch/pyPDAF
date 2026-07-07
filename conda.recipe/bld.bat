4d06cedb907ea8fb0f1abba9cc9cd5942b9c8ea8
Yumeng Chen <yumeng.chen@reading.ac.uk>
2026-07-07 15:51:29 +0100

    Fix conda.recipe for adding OpenMP and freethreading conda packages

--- conda.recipe/bld.bat
+++ conda.recipe/bld.bat
@@ -9,13 +9,10 @@ set OPENMP_ARGS=
 if /I "%openmp%"=="true" set OPENMP_ARGS=--config-settings=setup-args="-Dopenmp=true"
 if /I "%OPENMP%"=="true" set OPENMP_ARGS=--config-settings=setup-args="-Dopenmp=true"
 
-"%PYTHON%" -m pip install . -v --no-build-isolation^
-    -Cbuild-dir=build --config-settings=setup-args=^
-    "-Dlink_args_blas=-l"%LIBRARY_LIB%"\openblas.lib,"^
-    "-Dlink_args_blas=-l"%LIBRARY_LIB%"\FortranRuntime.lib,"^
-    "-Dlink_args_blas=-l"%LIBRARY_LIB%"\FortranDecimal.lib"^
-    --config-settings=setup-args="-Dincdirs="%LIBRARY_INC%^
-    --config-settings=setup-args="-Dlibdirs="%LIBRARY_LIB%^
-    --config-settings=setup-args="-Dmpi_mod="%LIBRARY_INC%"\mpi.f90"^
-    %OPENMP_ARGS%^
+"%PYTHON%" -m pip install . -v --no-build-isolation ^
+    -Cbuild-dir=build ^
+    --config-settings=setup-args="-Dlink_args_blas=-l"%LIBRARY_LIB%"\openblas.lib,-l"%LIBRARY_LIB%"\FortranRuntime.lib,-l"%LIBRARY_LIB%"\FortranDecimal.lib" ^
+    --config-settings=setup-args="-Dincdir_blas="%LIBRARY_INC%","%BUILD_PREFIX%"\Library\include" ^
+    --config-settings=setup-args="-Dmpi_mod=%LIBRARY_INC%\mpi.f90" ^
+    %OPENMP_ARGS% ^
     --config-settings=setup-args="-Dbuildtype=release"
--- conda.recipe/build.sh
+++ conda.recipe/build.sh
@@ -6,13 +6,13 @@ export FC=mpifort
 
 OPENMP_ARGS=()
 OPENMP_VARIANT="${openmp:-${OPENMP:-false}}"
-if [[ "${OPENMP_VARIANT,,}" == "true" ]]; then
+OPENMP_VARIANT_LOWER=$(printf '%s' "$OPENMP_VARIANT" | tr '[:upper:]' '[:lower:]')
+if [[ "$OPENMP_VARIANT_LOWER" == "true" ]]; then
     OPENMP_ARGS+=(--config-settings=setup-args="-Dopenmp=true")
 fi
 
 $PYTHON -m pip install . -v --no-build-isolation \
     --config-settings=setup-args="-Dlink_args_blas=-L"$PREFIX"/lib,-lopenblas" \
-    --config-settings=setup-args="-Dincdirs="$PREFIX"/include" \
-    --config-settings=setup-args="-Dlibdirs="$PREFIX"/lib" \
+    --config-settings=setup-args="-Dincdir_blas="$PREFIX"/include" \
     "${OPENMP_ARGS[@]}" \
     --config-settings=setup-args="-Dbuildtype=release"
